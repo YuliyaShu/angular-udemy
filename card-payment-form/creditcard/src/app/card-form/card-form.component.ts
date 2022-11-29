0588cb5da24145ede26599ca736d1bd48015001a
Yuliya_Shulgan <juliette7373@gmail.com>
2022-11-29 09:10:51 +0100

    feat: creditcard

---
+++ card-payment-form/creditcard/src/app/card-form/card-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { DateFormControl } from '../date-form-control';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(5)
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+      Validators.pattern(/[1-9]/)
+    ])
+  });
+  controlName = this.cardForm.get('name') as FormControl;
+  controlCardNumber = this.cardForm.get('cardNumber') as FormControl;
+  controlExpiration = this.cardForm.get('expiration') as FormControl;
+  controlSecurityCode = this.cardForm.get('securityCode') as FormControl;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  onResetClick() {
+    this.cardForm.reset();
+  }
+
+}
